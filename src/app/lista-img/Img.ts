4fe50228cba89f8215ca326118d3a152d20187b6
Gavi321 <78240363+Gavi321@users.noreply.github.com>
2022-06-03 17:13:50 +0100

    Delete src directory

--- src/app/lista-img/Img.ts
+++
@@ -1,9 +0,0 @@
-export class Img{
-    favorito: boolean;
-    params: any;
-
-    constructor(favorito: boolean, params: any){
-        this.favorito = favorito;
-        this.params = params;
-    }
-}
